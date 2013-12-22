fa93deda6f7f31baa78f30ad8ea822fce93ce121
KathrynKara <kathryn.dasalla@gmail.com>
2013-12-21 19:15:11 -0500

    add rails project

---
+++ app/controllers/final_controller.rb
@@ -0,0 +1,6 @@
+class FinalController < ApplicationController
+before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalApp::Application.config.secret_key_base = '1f7682239ecaa856d3a4b0ccb2283366a2000a2017cd103f1ca3b2b89da4eab33469a2ad1a0b6fa62d24830d488755feca9665fb4f76d9b540d6a347a03e69ef'
