ea692a8d8be92db40a7436f5c0b5c6d24d4caa4f
KathrynKara <kathryn.dasalla@gmail.com>
2013-12-29 16:00:45 -0500

    update edits

--- app/controllers/final_controller.rb
+++ app/controllers/final_controller.rb
@@ -2,5 +2,6 @@ class FinalController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @tags = Tag.all
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -18,4 +18,14 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find params[:id]
   end
+
+  def edit
+    @tag = Tag.find params[:id]
+  end
+
+  def update
+    safe_tag = params.require(:tag).permit(:title, :category, :comment)
+    @tag = current_user.tags.create safe_tag
+    redirect_to @tag, notice: "Happy Place successfully updated"
+  end
 end
